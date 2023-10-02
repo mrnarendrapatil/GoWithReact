b2559d60c1d7f455df2bfada3547a6084fbfd1c3
mrnarendrapatil <134691608+mrnarendrapatil@users.noreply.github.com>
2023-10-02 15:17:58 +0530

    Add files via upload

---
+++ NamasteReact/App.js
@@ -0,0 +1,7 @@
+const heading=document.createElement("h1",{},"Welcome namendra!");
+
+console.log(heading);
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
